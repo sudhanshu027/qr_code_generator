fdf2e5dc5486ac27757648db1116ba7f57b825f0
Sudhanshu Gupta <sgupta@opengov.com>
2021-08-28 12:45:11 +0530

    Added pg gem

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module QrGenerator
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+    # config.api_only = true
     
     config.paths.add File.join('app', 'services'), glob: File.join('**', '*.rb')
     config.autoload_paths += Dir[Rails.root.join('app', 'services')]
